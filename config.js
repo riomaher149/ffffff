408d704ca7c32b3826a215579f8a6a9b2edd60f2
riomaher149 <164175628+riomaher149@users.noreply.github.com>
2024-03-22 05:00:02 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bot: {
+    owners: ["1129563560444428288","994636641350197338","1013514773922582558","1146686725259796500","1140792973643227236","746096797973348352"],// owners id
+    botID: "1219799130751373442",// bot id
+
+    serverinvte: `https://discord.com/invite/HK5672Zs`, // Server invite url
+inviteBotUrl:"https://discord.com/oauth2/authorize?client_id=1219799130751373442&permissions=8&scope=bot",
+    clientSECRET: "P5OeHziynm6lKvM62jlZbszLRp5a8Ul2" , // Client Secret
+    callbackURL: "https://ee234fad-7713-4b21-8621-18e1e0c97b92-00-2ofjbvs48hm8p.pike.replit.dev/login", // Call Back Url
+    TOKEN: (process.env.token) // Bot Token
+  },
+  website: {
+    PORT: "3001",//website port
+  }
+}
